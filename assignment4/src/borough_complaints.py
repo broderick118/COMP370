132f2b6d3ef8af9e29749ae91ba3d46465ec6e06
Benjamin Roderick <benjamin.roderick@mail.mcgill.ca>
2024-10-02 03:49:31 +0000

    changes to borough_complaints still don't work

--- assignment4/src/borough_complaints.py
+++ assignment4/src/borough_complaints.py
@@ -1,6 +1,7 @@
 import os.path
 from argparse import ArgumentParser
 import pandas as pd # type: ignore
+from datetime import datetime
 
 def main():
     parser = ArgumentParser()
@@ -11,14 +12,19 @@ def main():
 
     args = parser.parse_args()
     filename = args.filename
-    startdate = args.startdate
-    enddate = args.enddate
+    startdate = pd.to_datetime(args.startdate, format="%m/%d/%y")
+    enddate = pd.to_datetime(args.enddate, format="%m/%d/%y")
+    print (startdate)
+    print (enddate)
     output = args.output
+    process_csv (filename, startdate, enddate, output)
 
 
 def process_csv (filename, startdate, enddate, output):
     filepath = os.path.dirname(__file__)[:-3] + "data/" + filename
-    dataframe = pd.read_csv(filepath)
+    dataframe = pd.read_csv(filepath, usecols=[1,2,5,25], names=["start", "end", "type", "borough"]).dropna()
+    date_filtered = dataframe[dataframe["start"] >= startdate] & dataframe[dataframe["end"] <= enddate]
+    print (date_filtered.head())
 
 if (__name__ == "__main__"):
     main()
