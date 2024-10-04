68bb4fd40609d7d596cbaa5affba50a12d0438a6
Benjamin Roderick <benjamin.roderick@mail.mcgill.ca>
2024-10-04 23:00:37 +0000

    filter_csv.py finished

--- assignment5/src/filter_csv.py
+++ assignment5/src/filter_csv.py
@@ -1,29 +1,21 @@
-import pandas as pd
+import pandas as pd # type: ignore
 import os.path
 from argparse import ArgumentParser
 
 def main():
     parser = ArgumentParser()
     parser.add_argument("-i", "--input", help="name of file", required=True)
-    parser.add_argument("-o", "--output", help="output file name")
-    parser.add_argument("-p", "--print", help="print head of output y/n", action="store_true")
     
     args = parser.parse_args()
     filename = args.input
-    print = args.print
-    output = args.output
-    do_operations(filename, print, output)
+    do_operations(filename)
 
-def do_operations(filename, output, print):
-    dirname = os.path.dirname(__name__)[:-3] + "data/"
-    df = pd.read_csv(dirname + filename, usecols=[5,6], names=["complaint_type", "descriptor"])
+def do_operations(filename):
+    dirname = os.path.dirname(__file__)[:-3] + "data/" + filename
+    with open (dirname, 'r') as data:
+        chunks = pd.read_csv(data, usecols=[5,6], names=["complaint_type", "descriptor"], iterator=True, chunksize=1000)
+        for chunk in chunks:
+            chunk.to_csv("out.csv", mode='a', index=False, header=False)
 
-    if print:
-        print(df.head())
-    elif output != None:
-        df.to_csv(dirname + output)
-    else :
-        df.to_csv(dirname + "csv_filtered.csv")
-
-if __name__ == "main":
+if __name__ == "__main__":
     main()
